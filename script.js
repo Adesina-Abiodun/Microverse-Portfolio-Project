ffe0da2a44e6ead1de016361b359531a3206b61c
Asum Victor <iamasum369@gmail.com>
2023-05-23 15:04:49 +0000

    Add comment to the script.js file

--- script.js
+++ script.js
@@ -3,16 +3,19 @@ const menuClose = document.querySelector('.close-btn');
 const menuLinks = document.querySelectorAll('.menu-links');
 const mobileMenu = document.querySelector('.mobile-menu');
 
+//event to show mobile menu links when user clicks on the humberger icon
 hamburger.addEventListener('click', () => {
   document.body.style.overflow = 'hidden';
   mobileMenu.classList.add('openMenu');
 });
 
+//event to close mobile menu links when user clicks on the close button
 menuClose.addEventListener('click', () => {
   mobileMenu.classList.remove('openMenu');
   document.body.style.overflow = 'scroll';
 });
 
+//event to close mobile menu links when user clicks on any of the menu links
 for (let i = 0; i < menuLinks.length; i += 1) {
   menuLinks[i].addEventListener('click', () => {
     mobileMenu.classList.remove('openMenu');
